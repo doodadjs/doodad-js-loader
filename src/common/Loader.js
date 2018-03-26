af9efdceafcbef9907c3b31b521cee826945dcdd
Claude Petit <doodadjs@gmail.com>
2018-03-26 17:17:20 -0400

    Rename "DD_MODULES" to "modules".

--- src/common/Loader.js
+++ src/common/Loader.js
@@ -29,9 +29,9 @@
 	"use strict";
 //! END_IF()
 
-exports.add = function add(DD_MODULES) {
-	DD_MODULES = (DD_MODULES || {});
-	DD_MODULES['Doodad.Loader'] = {
+exports.add = function add(modules) {
+	modules = (modules || {});
+	modules['Doodad.Loader'] = {
 		version: /*! REPLACE_BY(TO_SOURCE(VERSION(MANIFEST("name")))) */ null /*! END_REPLACE()*/,
 		create: function create(root, /*optional*/_options, _shared) {
 			//===================================
@@ -617,7 +617,7 @@ exports.add = function add(DD_MODULES) {
 			//};
 		},
 	};
-	return DD_MODULES;
+	return modules;
 };
 
 //! END_MODULE()
